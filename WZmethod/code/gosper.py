3cc6ef94d5a382cd5bcf2658f3553b98695746b0
LarsAstrom <astrom.lars@telia.com>
2019-10-10 15:22:12 +0200

    gosper working ?!?!

--- WZmethod/code/gosper.py
+++ WZmethod/code/gosper.py
@@ -5,32 +5,37 @@ sys.setrecursionlimit(10**6)
 def gosper(num,den,variable):
     q,r = num,den
     p = polynomial([constant(1)],'n')
-    rj = parse(r.to_string().replace(variable,'({}+j)'.format(variable)))
-    q.gcd(rj).PRINT()
-    while not q.gcd(rj).is_constant:
-        g = q.gcd(rj)
-        zeros = g.get_zeros('j')
-        if type(zeros) == str:
-            break
-        if not zeros:
-            break
-        MAX = max(zeros)
-        if MAX < 0:
-            break
-        rmax = parse(r.to_string().replace(variable,'({}+{})'.format(variable,MAX)))
-        if not q.divide(rmax)[1].is_zero:
-            break
-        g = q.gcd(rmax).divide(q.divide(rmax)[2])[0]
+    zero = get_common_factor(q,r,variable)
+    while zero != None:
+        g = q.gcd(parse(r.to_string().replace(variable,'({}+{})'.format(variable,zero))))
         q = q.divide(g)[0]
-        r = r.divide(parse(g.to_string().replace(variable,'({}-{})'.format(variable,MAX))))[0]
-        for i in range(MAX):
+        r = r.divide(parse(g.to_string().replace(variable,'({}-{})'.format(variable,zero))))[0]
+        for i in range(zero):
             p = p.multiply(parse(g.to_string().replace(variable,'({}-{})'.format(variable,i))))
-        rj = parse(r.to_string().replace(variable,'({}+j)'.format(variable)))
+        zero = get_common_factor(q,r,variable)
     return q,r,p
 
+def get_common_factor(q,r,variable):
+    rj = parse(r.to_string().replace(variable,'({}+j)'.format(variable)))
+    #Want to find j such that gcd between q and rj is not 1.
+    return find_zeros(q,rj)
+
+def find_zeros(q,rj):
+    if rj.is_zero: return None
+    zeros = rj.get_zeros('j')
+    if zeros and max(zeros) >= 0: return max(zeros)
+    return find_zeros(rj,q.modulo(rj))
+
 def test_gosper(num,den,variable='k'):
+    if num == '' or den == '':
+        print('==============================')
+        print('Testing Gosper does not work for numerator and denominator\n{}\n{}'.format(num,den))
+        print('==============================')
+        return
     print('======TESTING GOSPER======')
     p1,p2 = parse(num),parse(den)
+    print(num)
+    print(den)
     print('Numerator: {}'.format(p1.to_string()))
     print('Denominator: {}'.format(p2.to_string()))
     q,r,p = gosper(p1,p2,variable)
@@ -45,63 +50,71 @@ def test_gosper(num,den,variable='k'):
 def print_break():
     print('========================================')
 
-if __name__ == '__main__':
-    '''
-    s7 = '2(3k-2n-2)(n+2-k)'
-    s8 = 'k(3k-2n-5)'
-    test_gosper(s7,s8)
+def test_get_common_factor(s1,s2):
+    print(get_common_factor(parse(s1),parse(s2),'k'))
 
-    s5 = 'k-1'
-    s6 = 'k+1'
-    test_gosper(s5,s6)
+if __name__ == '__main__':
+    #01#$\sum_{k=0}^n \binom{n}{k} = 2^n$
+    s01 = '(2k-n-1)(n+2-k)'
+    s02 = 'k(2k-n-3)'
+    #02#$\sum_{k=0}^n (-1)^k\binom{n}{k}\binom{2k}{k}4^{n-k}=\binom{2n}{n}$
+    s03 = '-(2k-1)(2nk+n+k+1)(n+2-k)'
+    s04 = '2k^2(2nk-n+k)'
+    #03#$\sum_{k=0}^n \binom{n}{k}^2 = \binom{2n}{n}$
+    s05 = '(n+2-k)^2'
+    s06 = 'k^2'
+    #04#$\sum_{k=-n}^n (-1)^k\binom{2n}{n+k}^3 = \frac{(3n)!}{n!}$
+    s07 = ''
+    s08 = ''
+    #05#$\sum_{k=0}^n 2^k\binom{n}{k} = 3^n$
+    s09 = '2(3k-2n-2)(n+2-k)'
+    s10 = 'k(3k-2n-5)'
+    #06#$\sum_{k=0}^n k\binom{n}{k} = n2^{n-1}$
+    s11 = '(n+2-k)(2k-n-2)'
+    s12 = '(k-1)(2k-n-4)'
+    #07#$\sum_{k=1}^n \frac{1}{k(k-1)} = 1-\frac{1}{n}$
+    s13 = 'k-1'
+    s14 = 'k+1'
+    #08#$\sum_{k=0}^n \binom{k}{c} = \binom{n+1}{c+1}$
+    s15 = 'k'
+    s16 = 'k-c'
+    #09#$\sum_{k=0}^n \binom{r+k}{k} = \binom{r+n+1}{n}$
+    s17 = 'r+k'
+    s18 = 'k'
+    #10#$\sum_{k=0}^n \binom{m-k}{n-k} = \binom{m+1}{n}$
+    s19 = '(kn+1-km-k-n)(n+2-k)'
+    s20 = '(kn+2+m-km-k-2n)(m+1-k)'
+
     '''
-    s1 = '(2k-n-1)(n+2-k)'
-    s2 = 'k(2k-n-3)'
-    p1 = parse(s1)
-    p2 = parse(s2)
-    p3 = parse(s2.replace('k','(k+1)'))
-    p4 = parse(s2.replace('k','(k+j)'))
-    p1.PRINT()
-    p2.PRINT()
-    p3.PRINT()
-    p4.PRINT()
-    p1.gcd(p2).PRINT()
-    p1.gcd(p3).PRINT()
-    p1.gcd(p4).PRINT()
-    p2.gcd(p3).PRINT()
-    p2.gcd(p4).PRINT()
-    p3.gcd(p4).PRINT()
-    print_break()
-    a,b,c = p1.divide(p4)
-    a.PRINT()
-    b.PRINT()
-    c.PRINT()
-    print_break()
-    a,b,c = p4.divide(b)
-    a.PRINT()
-    b.PRINT()
-    c.PRINT()
-    test_gosper(s1,s2)
-    '''
+    What are these?!?!?!
     s3 = '(n+2-k)^2((n+1)^3-2(n+1-k)(2n+1))'
     s4 = 'k^2((n+1)^3-2(n+2-k)^2(2n+1))'
-    #test_gosper(s3,s4)
-
-    p1, p2 = parse(s3), parse(s4)
-    p3 = parse(s4.replace('k','(k+j)'))
-    p1.PRINT()
-    p3.PRINT()
-    print_break()
-    a,b,c = p1.divide(p3)
-    a.PRINT()
-    b.PRINT()
-    c.PRINT()
-    print_break()
-    p3.PRINT()
-    b.PRINT()
-    print_break()
-    a,b,c = p3.divide(b)
-    a.PRINT()
-    b.PRINT()
-    c.PRINT()
     '''
+    tests = [(s01,s02),(s03,s04),(s05,s06),(s07,s08),(s09,s10),\
+            (s11,s12),(s13,s14),(s15,s16),(s17,s18),(s19,s20)]
+
+    for test in tests:
+        try:
+            test_get_common_factor(*test)
+        except:
+            print('==============================')
+            print(test[0])
+            print(test[1])
+            print('DOES NOT WORK GET COMMON FACTOR')
+            print('==============================')
+
+    for test in tests:
+        try:
+            test_gosper(*test)
+        except:
+            print('==============================')
+            print(test[0])
+            print(test[1])
+            print('DOES NOT WORK GOSPER')
+            print('==============================')
+
+    exit()
+    test_gosper(s1,s2)
+    #test_gosper(s3,s4)
+    test_gosper(s5,s6)
+    test_gosper(s7,s8)
