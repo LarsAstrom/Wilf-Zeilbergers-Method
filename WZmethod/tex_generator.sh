5d8bedb8d255e689d4c9d3c8030d9a4d7c0ced6a
LarsAstrom <astrom.lars@telia.com>
2019-10-23 10:58:01 +0200

    splitted main into parts

--- WZmethod/tex_generator.sh
+++ WZmethod/tex_generator.sh
@@ -16,5 +16,17 @@ else
   rm *.blg
   rm *.dvi
   rm *latexmk
+  rm texs/*.thm
+  rm texs/*.toc
+  rm texs/*.out
+  rm texs/*.aux
+  rm texs/*.log
+  rm texs/*.bcf
+  rm texs/*.xml
+  rm texs/*.fls
+  rm texs/*.bbl
+  rm texs/*.blg
+  rm texs/*.dvi
+  rm texs/*latexmk
   open $1".pdf"
 fi
