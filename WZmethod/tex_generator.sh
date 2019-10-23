42faa4933048e2df03a5ba8ed202a335d8e82051
LarsAstrom <astrom.lars@telia.com>
2019-10-23 10:36:55 +0200

    started using template from LTH for report, started report

--- WZmethod/tex_generator.sh
+++ WZmethod/tex_generator.sh
@@ -3,11 +3,18 @@ then
   echo "Number of arguments has to be 1."
   exit 1
 else
-  pdflatex "texs/"$1".tex"
+  latexmk -pdf "texs/"$add_string$1".tex"
+  rm *.thm
+  rm *.toc
   rm *.out
   rm *.aux
   rm *.log
   rm *.bcf
   rm *.xml
+  rm *.fls
+  rm *.bbl
+  rm *.blg
+  rm *.dvi
+  rm *latexmk
   open $1".pdf"
 fi
