8ef7907f4156398a7a0c32fd46a5b509e07c978d
LarsAstrom <astrom.lars@telia.com>
2019-12-04 15:43:38 +0100

    read through v2.0, -> 2.1

--- WZmethod/tex_generator.sh
+++ WZmethod/tex_generator.sh
@@ -1,56 +1,16 @@
-if $# != 1
+bash clean.sh
+if [ "$#" != "1" ];
 then
-  echo "Number of arguments has to be 1."
-  exit 1
+  if [ "$#" != "2" ];
+  then
+    echo "Number of arguments has to be 1."
+    exit 1
+  else
+    latexmk -pdf -shell-escape $2"/"$1".tex"
+    open $1".pdf"
+  fi
 else
-  rm *.thm
-  rm *.toc
-  rm *.out
-  rm *.aux
-  rm *.log
-  rm *.bcf
-  rm *.xml
-  rm *.fls
-  rm *.bbl
-  rm *.blg
-  rm *.dvi
-  rm *latexmk
-  rm texs/*.thm
-  rm texs/*.toc
-  rm texs/*.out
-  rm texs/*.aux
-  rm texs/*.log
-  rm texs/*.bcf
-  rm texs/*.xml
-  rm texs/*.fls
-  rm texs/*.bbl
-  rm texs/*.blg
-  rm texs/*.dvi
-  rm texs/*latexmk
-  latexmk -pdf -shell-escape "texs/"$add_string$1".tex"
-  rm *.thm
-  rm *.toc
-  rm *.out
-  rm *.aux
-  rm *.log
-  rm *.bcf
-  rm *.xml
-  rm *.fls
-  rm *.bbl
-  rm *.blg
-  rm *.dvi
-  rm *latexmk
-  rm texs/*.thm
-  rm texs/*.toc
-  rm texs/*.out
-  rm texs/*.aux
-  rm texs/*.log
-  rm texs/*.bcf
-  rm texs/*.xml
-  rm texs/*.fls
-  rm texs/*.bbl
-  rm texs/*.blg
-  rm texs/*.dvi
-  rm texs/*latexmk
+  latexmk -pdf -shell-escape "texs/"$1".tex"
   open $1".pdf"
 fi
+bash clean.sh
